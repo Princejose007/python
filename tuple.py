602863cdb1390702e993c985fcec8de9aa964c13
Princejose007 <125237823+Princejose007@users.noreply.github.com>
2025-07-25 15:51:26 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,65 @@
+# immutable / cannot be change (only by converting into list)
+fruits=("apple","bannana","orange")
+# print(fruits)
+
+
+
+#single ten tuple/tuple with ony one value
+
+# fruit=("apple",)
+# print(type(fruit))
+
+#accesing tuple
+
+# print(fruits[1])
+
+#negative accesing
+
+# print(fruits[-1])
+
+#updating tuple values
+
+# y=list(fruits)
+# y[1]="kiwi"
+# x=tuple(fruits)
+# print(fruits)
+
+
+#add items
+# y=list(fruits)
+# y.append("cherry")
+# fruits=tuple(y)
+# print(fruits)
+
+
+#without converting method
+
+# y=("cherry",)
+# fruits +=y
+# print(fruits)
+
+
+#Removing
+
+# y=list(fruits)
+# y.remove("apple")
+# fruits=tuple(y)
+# print(fruits)
+
+#del funnction
+# del fruits
+# print(fruits)
+
+
+#loop in tuple
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+# tuple2=(1,2,3)
+# tuple3=fruits+tuple2
+# print(tuple3)
+
+# multiply=fruits*2
+# print(multiply)
+
