0548d52ca20e9cb9f4a4736d7e604ace27d69f9e
Princejose007 <125237823+Princejose007@users.noreply.github.com>
2025-07-26 11:32:43 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,98 @@
+#no position,(if we print no order randomly print)
+#it is immutable/change
+#Duplicate not allowed
+#if there is same variable repeate the last one of reaoetde is printed
+
+# fruits={"apple","bannana","cherry",False,True,0}
+# print(fruits)#the false and 0 is same 
+
+
+
+# fruits={"apple","bannana","cherry"}
+# print("bannana" in fruits) #output ---> True
+
+
+#Adding new
+# fruits={"apple","orange","cherry"}
+# fruits.add("orange")
+# print(fruits)#The new ones position is random
+
+#updating
+fruits={"apple","bannana","cherry"}
+# veges={"bettroot","carrot","cabbage"}
+# fruits.update(veges)
+# print(fruits)
+
+#Remove
+# fruits.remove("bannana")
+# print(fruits)
+
+
+
+#discard()
+# fruits.discard("bannana")
+# print(fruits)
+
+#pop()
+
+# x=fruits.pop()
+# print(x)
+# print(fruits)  #it randomly remove the variables each time diffrent
+
+
+#clear()
+
+
+#it clear all the elements the set still remain
+
+# fruits.clear()
+# print(fruits)
+
+#del funnction
+# del function is used to copmletely delect the set
+# del fruits
+# print(fruits)
+
+#loop set
+
+# for x in fruits:
+#     print(x)
+
+#join sets
+
+# set1={"d","b","c"}
+# set2={1,2,3}
+# set=set1|set2
+# print(set)
+
+#intersection 
+set1={"apple","bannana","cherry"}
+set2={"google","microsoft","apple"}
+# set=set1.intersection(set2)
+# set=set1 & set2
+
+# print(set)
+
+
+#diffrence 
+
+#set=set1 - set2
+
+# set=set1.difference(set2)
+# print(set)
+
+#symmetric
+# set=set1.symmetric_difference(set2)
+# print(set)
+
+#disjoint --> function using is isdisjoint()
+
+# z=set1.isdisjoint(set2)
+# print(z)
+
+
+
+
+
+
+
