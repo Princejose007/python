4f9142dd48565e82afb3fd1d00198c0042492ef5
Prince jose <125237823+Princejose007@users.noreply.github.com>
2025-07-30 11:05:17 +0530

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,14 @@
+#Decorator
+
+
+
+# def make_pretty(func):
+#     def inner():
+#         print("i get decorated ")
+#         func()
+#     return inner
+
+# @make_pretty
+# def ordinary():
+#     print("I am ordinary")
+# ordinary()
