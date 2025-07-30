0d822b03cb09cf59d5de7c417fe15cdf1470aca2
Prince jose <125237823+Princejose007@users.noreply.github.com>
2025-07-30 11:04:53 +0530

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,34 @@
+#generator /it is a function used to iterate liek loop we can use this isted of loop
+
+
+
+# def number():  #number is generator name
+#      yield 1
+#      yield 2
+#      yield 3
+
+# for value in number():
+#      print(value)
+
+
+#example for using yield and next()
+
+# def simple_generator():
+#     yield "Hello"
+#     yield "world"
+
+# gen = simple_generator()
+# print(next(gen))
+# print(next(gen))
+
+# by using while
+
+# def my_generator(n):
+#     value=0
+#     while value <n:
+#         yield value
+#         value+=1
+# for value in my_generator(3):
+#     print(value)
+
+
